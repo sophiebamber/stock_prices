f6da3f19bc12ea86807a2f0ccf54569b80529cc9
Sophie <sophiebamber1029@gmail.com>
2020-09-14 09:39:54 +0100

    Made code into function
    - created function which can get stock price of any yahoo stock page

--- importer.py
+++ importer.py
@@ -1,10 +1,27 @@
 import bs4
 import requests
-from datetime import datetime
+from typing import Optional
 
-r = requests.get('https://uk.finance.yahoo.com/quote/%5EVWRL/')
 
-soup = bs4.BeautifulSoup(r.text, 'lxml')
+def get_stock_price(url: str, html_object: str) -> Optional[str]:
+    
+    """
+    Gets the stock price from any yahoo stock page
+    :param url: url to website which describes the stock
+    :param html_object: description of the html object
+    :return: latest price of stock 
+    """
 
-price = soup.find_all('div', {'class': 'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
+    try:
+
+        r = requests.get(url)
+        soup = bs4.BeautifulSoup(r.text, 'lxml')
+        stock_price = soup.find_all('div', {'class': html_object})[0].find('span').text
+
+        return stock_price
+    except Exception as e:
+        return None
+
+
+price = get_stock_price('https://uk.finance.yahoo.com/quote/%5EVWRL/', 'My(6px) Pos(r) smartphone_Mt(6px)')
 print(price)
